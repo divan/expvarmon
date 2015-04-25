ee578240c7ebeabf979120396f3b79debacc2c70
Ivan Daniluk <ivan.daniluk@gmail.com>
2015-04-25 09:06:45 +0300

    Added test for stack

--- stack.go
+++ stack.go
@@ -1,10 +1,12 @@
 package main
 
+// Stack is a limited FIFO for holding sparkline values.
 type Stack struct {
 	Values []int
 	Len    int
 }
 
+// NewStack inits new Stack with size limit.
 func NewStack(size int) *Stack {
 	return &Stack{
 		Values: make([]int, size),
@@ -12,6 +14,7 @@ func NewStack(size int) *Stack {
 	}
 }
 
+// Push inserts data to stack, preserving constant length.
 func (s *Stack) Push(val int) {
 	s.Values = append(s.Values, val)
 	if len(s.Values) > s.Len {
---
+++ stack_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestStack(t *testing.T) {
+	size := 10
+	s := NewStack(size)
+
+	for i := 0; i < size+5; i++ {
+		s.Push(i)
+		l := len(s.Values)
+
+		if l < size {
+			if l != i+1 {
+				t.Fatal("len is incorrect. expecting %d, got %d", i, l)
+			}
+		} else {
+			if l != size {
+				t.Fatal("len is incorrect. expecting %d, got %d", size, l)
+			}
+		}
+	}
+}
