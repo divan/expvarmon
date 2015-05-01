4a5ef311a6ee356ae94b62e6b554d4d3408fa5de
Ivan Daniluk <ivan.daniluk@gmail.com>
2015-05-01 20:28:51 +0300

    Improved tests

--- expvars_test.go
+++ expvars_test.go
@@ -45,29 +45,25 @@ func TestExpvarsAdvanced(t *testing.T) {
 		t.Fatalf("cannot open test file %v", err)
 	}
 	defer file.Close()
-	/*
-		vars, err := ParseExpvar(file)
-		if err != nil {
-			t.Fatal(err)
-		}
 
-		if len(vars.Extra) != 2 {
-			t.Fatalf("Got:", vars)
-			t.Fatalf("vars should have 2 items, but has %d", len(vars.Extra))
-		}
+	expvar, err := ParseExpvar(file)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		if int(vars.Extra["goroutines"].(float64)) != 10 {
-			t.Logf("Expecting 'goroutines' to be %d, but got %d", 10, vars.Extra["goroutines"])
-		}
+	goroutines, err := expvar.GetInt64("goroutines")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if goroutines != 10 {
+		t.Fatalf("Expecting 'goroutines' to be %d, but got %d", 10, goroutines)
+	}
 
-		counters := vars.Extra["counters"].(map[string]interface{})
-		counterA := counters["A"].(float64)
-		counterB := counters["B"].(float64)
-		if counterA != 123.12 {
-			t.Logf("Expecting 'counter.A' to be %f, but got %f", 123.12, counterA)
-		}
-		if int(counterB) != 245342 {
-			t.Logf("Expecting 'counter.B' to be %d, but got %d", 245342, counterB)
-		}
-	*/
+	counter_a, err := expvar.GetFloat64(VarName("counters.A").ToSlice()...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if counter_a != 123.12 {
+		t.Fatalf("Expecting 'counters.A' to be %f, but got %f", 123.12, counter_a)
+	}
 }
