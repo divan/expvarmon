ce7ce63f13d30d597c0800b19c75436ca732e3f9
Ivan Daniluk <ivan.daniluk@gmail.com>
2015-05-02 10:56:55 +0300

    Code moving

--- utils.go
+++ utils.go
@@ -30,3 +30,13 @@ func BaseCommand(cmdline []string) string {
 	}
 	return filepath.Base(cmdline[0])
 }
+
+// ParsePorts converts comma-separated ports into strings slice
+func ParsePorts(s string) ([]string, error) {
+	ports := strings.FieldsFunc(s, func(r rune) bool { return r == ',' })
+	if len(ports) == 0 {
+		return nil, errors.New("no ports specified")
+	}
+
+	return ports, nil
+}
